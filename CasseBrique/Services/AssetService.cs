9637ef1e094251409e4c0fd9b8849563b713bc1c
Cédric_FIXE <thiriet.remondin.cedric@gmail.com>
2024-06-03 19:36:32 +0200

    ServiceLocator + AssetManager

---
+++ CasseBrique/Services/AssetService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasseBrique.Services
+{
+    public class AssetService
+    {
+        Dictionary<string, object> _assets = new Dictionary<string, object>();
+
+        public AssetService()
+        {
+            ServiceLocator.Register<AssetService>(this);
+        }
+
+        public void Load<T>(string name) => _assets[name] = ServiceLocator.Get<ContentManager>().Load<T>(name);
+
+        public T Get<T>(string name)
+        {
+            if (!_assets.ContainsKey(name))
+                throw new InvalidOperationException($"AssetService : Asset {name} does not exist");
+            return (T)_assets[name];
+        }
+    }
+}
---
+++ CasseBrique/Services/ServiceLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CasseBrique.Services
+{
+    public static class ServiceLocator
+    {
+        private static Dictionary<Type, object> _services = new Dictionary<Type, object>();
+
+        public static void Register<T>(T service)
+        {
+            _services[typeof(T)] = service;
+        }
+
+        public static T Get<T>()
+        {
+            if (!_services.ContainsKey(typeof(T)))
+                throw new InvalidOperationException($"Service of type {typeof(T)} does not exist");
+            return (T)_services[typeof(T)];
+        }
+    }
+}
