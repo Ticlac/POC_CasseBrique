2bc3b2d933df7f7036272c9fe6c9fd2ede2a9c62
Cédric_FIXE <thiriet.remondin.cedric@gmail.com>
2024-06-09 14:51:01 +0200

    refactorisation de la scene : Chaque scene possède sa liste de gameObjects

---
+++ CasseBrique/GameObjects/GameObject.cs
@@ -0,0 +1,51 @@
+﻿using CasseBrique.Scenes;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasseBrique.GameObjects
+{
+    public abstract class GameObject
+    {
+        private bool enable;
+        public bool isFree { get; private set; }
+        public Scene root { get; private set; }
+        public bool Enable { 
+            get { return enable;} 
+            set {
+                if (enable != value)
+                {
+                    enable = value;
+                    if (enable) OnEnable();
+                    else OnDisable();
+                }
+            }
+        }
+
+        public GameObject(bool enable, Scene root)
+        {
+            this.enable = enable;
+            this.root = root;
+        }
+
+        public virtual void Update(float dt)
+        {
+
+        }
+        public virtual void Draw(SpriteBatch sb)
+        {
+
+        }
+
+        protected virtual void OnEnable() { }
+        protected virtual void OnDisable() { }
+        protected virtual void Start() { }
+        public virtual void OnFree() { }
+    }
+}
--- CasseBrique/Scenes/Scene.cs
+++ CasseBrique/Scenes/Scene.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using CasseBrique.GameObjects;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,25 +8,56 @@ using System.Threading.Tasks;
 
 namespace CasseBrique.Scenes
 {
-    public class Scene
+    public abstract class Scene
     {
+        private List<GameObject> gameObjects = new List<GameObject>();
+        public virtual void Load() { }
 
-        public virtual void Load()
+        public virtual void Unload() { }
+
+        public void Update(float dt)
         {
+            foreach(var obj in gameObjects)
+                if (obj.Enable)
+                    obj.Update(dt);
+
+
+            for (int i = gameObjects.Count - 1; i >= 0; i--)
+            {
+                if (gameObjects[i].isFree)
+                {
+                    gameObjects[i].OnFree();
+                    gameObjects.RemoveAt(i);
+                }
+            }
 
         }
-        public virtual void Update(float dt)
+        public void Draw(SpriteBatch sb)
         {
-
+            foreach (var obj in gameObjects)
+            {
+                if (obj.Enable)
+                    obj.Draw(sb);
+            }
         }
-        public virtual void Draw(SpriteBatch sb)
+
+        public void AddGameObject(GameObject obj)
         {
-
+            obj.Start();
+            gameObjects.Add(obj);
         }
 
-        public virtual void Unload()
+        public List<T> GetGameObjects<T>()
         {
+            var returnGameObjects = new List<T>();
 
+            foreach (GameObject gameObject in gameObjects)
+                // si gameobject est de type T ==> typedGameObject = T
+                if (gameObject is T typedGameObject)
+                    returnGameObjects.Add(typedGameObject);
+
+            return returnGameObjects;
         }
+
     }
 }
