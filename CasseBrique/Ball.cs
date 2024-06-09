f60628aada706050a3b33009f472547bfc593bab
Cédric_FIXE <thiriet.remondin.cedric@gmail.com>
2024-06-09 16:09:49 +0200

    Refactorisation de Ball : Héritage SpriteGameObject

--- CasseBrique/Ball.cs
+++ CasseBrique/Ball.cs
@@ -1,37 +1,73 @@
-﻿using CasseBrique.Services;
+﻿using CasseBrique.GameObjects;
+using CasseBrique.Scenes;
+using CasseBrique.Services;
 using CasseBrique.Services.Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace CasseBrique
 {
-    public class Ball
+    public class Ball : SpriteGameObject
     {
-        private Vector2 position;
-        private Texture2D texture;
         private Vector2 velocity;
-        private Color color;
+        private Vector2 direction;
+        private float speed;
+        private Rectangle bounds;
+        private float radius => texture.Width *.5f;
 
-        public Ball()
+        public Ball(Scene root, Vector2 direction, Rectangle bounds) : base(root)
         {
-            this.texture = ServiceLocator.Get<IAssetService>().Get<Texture2D>("Ball");
-            this.position = ServiceLocator.Get<IScreenService>().Center;
-            this.color = Color.White;
+            texture = ServiceLocator.Get<IAssetService>().Get<Texture2D>("Ball");
+            position = ServiceLocator.Get<IScreenService>().Center;
+            offset = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+            this.bounds = bounds;
+            this.direction = direction;
+            this.speed = 400f;
+
         }
 
-        public void Shoot(Vector2 direction, float speed)
+        public override void Update(float dt)
         {
+            Move(dt);
+            Bounce();
+        }
+
+        //public override void Draw(SpriteBatch sb)
+        //{
+        //    sb.Draw(texture, position, null, color, rotation, offset, scale, SpriteEffects.None, 0);
+        //}
+
+        public void Move(float dt)
+        {
+            //Remettre dans un cercle de rayon 1
+            direction.Normalize();
             velocity = direction * speed;
-        }
-
-        public void Update(float dt)
-        {
             position += velocity * dt;
         }
 
-        public void Draw(SpriteBatch sb)
+        public void Bounce()
         {
-            sb.Draw(texture, position, color);
+            if (position.X > bounds.Right - radius)
+            {
+                position.X = bounds.Right - radius;
+                direction.X *= -1;
+            }
+            else if(position.X < bounds.Left + radius)
+            {
+                position.X = bounds.Left + radius;
+                direction.X *= -1;
+            }
+            if (position.Y > bounds.Bottom - radius)
+            {
+                position.Y = bounds.Bottom - radius;
+                direction.Y *= -1;
+            }
+            else if (position.Y < bounds.Top + radius)
+            {
+                position.Y = bounds.Top + radius;
+                direction.Y *= -1;
+            }
         }
+
     }
 }
