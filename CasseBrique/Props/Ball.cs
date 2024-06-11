dd38d4df139e7af9a0e8787f074d0482f3b79c13
Cédric_FIXE <thiriet.remondin.cedric@gmail.com>
2024-06-12 00:12:52 +0200

    Fonction générique de collision

--- CasseBrique/Props/Ball.cs
+++ CasseBrique/Props/Ball.cs
@@ -45,6 +45,51 @@ namespace CasseBrique.Props
                 // TODO :
                 // envoyer un message outOfBounds
                 // recoller la balle au pad
+
+        private void checkCollisionWithObjects<T>() where T : SpriteGameObject
+        {
+            // fonction de la class Scene
+            var objects = root.GetGameObjects<T>();
+
+            foreach (SpriteGameObject obj in objects)
+            {
+                if (obj.Enable && obj.collider.Intersects(this.collider))
+                {
+                    // verification du sens de la collision
+                    float deathX = Math.Min(this.collider.Right - obj.collider.Left, obj.collider.Right - this.collider.Left);
+                    float deathY = Math.Min(this.collider.Bottom - obj.collider.Top, obj.collider.Bottom - this.collider.Top);
+
+                    //check dou provient la collision
+                    if (deathX < deathY)
+                    {
+                        if (this.collider.Right > obj.collider.Left && this.collider.Left < obj.collider.Right)
+                        {
+                            this.position.X = obj.collider.Left - this.offset.X;
+                            this.direction.X *= -1;
+                        }
+                        else if (this.collider.Left < obj.collider.Right && this.collider.Right > obj.collider.Left)
+                        {
+                            this.position.X = obj.collider.Right - this.offset.X;
+                            this.direction.X *= -1;
+                        }
+                    }
+                    else
+                    {
+
+                        if (this.collider.Bottom > obj.collider.Top && this.collider.Top < obj.collider.Bottom)
+                        {
+                            this.position.Y = obj.collider.Top - this.offset.Y;
+                            this.direction.Y *= -1;
+                        }
+                        else if (this.collider.Top < obj.collider.Bottom && this.collider.Bottom > obj.collider.Top)
+                        {
+                            this.position.Y = obj.collider.Bottom - this.offset.Y;
+                            this.direction.Y *= -1;
+                        }
+                    }
+                    this.direction = Vector2.Normalize(this.direction);
+                    obj.OnCollide(this);
+                }
             }
         }
 
